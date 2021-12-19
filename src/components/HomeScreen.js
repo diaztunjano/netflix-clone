b55f4842c791fd24cd29a4dff41b783ba61c41e2
diaztunjano <dfdiaz1@uc.cl>
2021-12-19 14:41:55 -0300

    feat(row): Add row component receiving  fetched data

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -2,7 +2,8 @@ import React from "react";
 import "../styles/HomeScreen.css";
 import Nav from "./Nav.js";
 import Banner from "./Banner.js";
-
+import Row from "./Row";
+import requests from "../helpers/Requests";
 
 function HomeScreen() {
   return (
@@ -10,9 +11,21 @@ function HomeScreen() {
       {/* Nav */}
       <Nav />
       {/* Banner */}
-      <Banner/>
+      <Banner />
 
       {/* Row */}
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetlfixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
